162b928232cc29068e54b8dae38106bbe2d5a2da
Samuel <Hebsibal.Samuel@nielseniq.com>
2022-05-25 16:49:05 +0530

    adding api

--- routes/user.js
+++ routes/user.js
@@ -1,4 +1,5 @@
 const router = require("express").Router();
+const { domainToASCII } = require("url");
 const User = require("../models/user");
 
 
@@ -6,8 +7,22 @@ router.get("/", async (req, res) => {
     const query = req.query.new;
     try {
         const users = await User.find()
-        // const cart = await User.findOne({ userName: req.params.userId });
         res.status(200).json(users);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.post("/login", async (req, res) => {
+    try {
+        let username = req.body.userName;
+        let password = req.body.password;
+        let canLogIn = false
+        const users = await User.findOne({ userName: username })
+        if(users && users.password==password){
+            canLogIn = true;
+        }
+        res.status(200).json(canLogIn);
     } catch (err) {
         res.status(500).json(err);
     }
